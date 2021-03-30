1beb06eb045b63ad2efdef52c0f59deeca1f75db
Tim Corley <tcorley215@gmail.com>
2021-03-30 10:40:19 -0400

    add nodemailer
    move form to a component

---
+++ components/ContactForm.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-quicksand font-bold leading-8 tracking-tight">
+        Get in touch...
+      </h1>
+      <form className="w-full max-w-lg">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label htmlFor="name" className="text-2xl font-quicksand font-bold">
+              Name
+            </label>
+            <input
+              aria-label="Name Input"
+              type="text"
+              placeholder="Your Name"
+              className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label htmlFor="name" className="text-2xl font-quicksand font-bold">
+              Email
+            </label>
+            <input
+              aria-label="Email Input"
+              type="mail"
+              placeholder="Your Email Address"
+              className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+            />
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label htmlFor="name" className="text-2xl font-quicksand font-bold">
+              Message
+            </label>
+            <textarea
+              aria-label="Message Input"
+              placeholder="Your Message"
+              className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-300 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+            />
+          </div>
+        </div>
+
+        <div className="flex">
+          <button
+            className="bg-blue-500 hover:bg-blue-600 dark:hover:bg-blue-400 text-white text-2xl font-quicksand font-bold py-2 px-4 rounded-lg"
+            type="button"
+          >
+            Send Message
+          </button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default ContactForm
