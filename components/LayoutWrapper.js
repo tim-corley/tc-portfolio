aa6383ca53fe7fad29f5893b4652d5d5ae4aeba5
Tim Corley <tcorley215@gmail.com>
2021-04-08 11:38:41 -0400

    adjust home subtitle text justification on mobile
    hide google recaptcha badge
    
    make dark/light more logo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,22 +1,25 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import LightLogo from '@/data/logo-light.svg'
+import DarkLogo from '@/data/logo-dark.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import { useTheme } from 'next-themes'
 
 const LayoutWrapper = ({ children }) => {
+  const { theme, setTheme } = useTheme()
   return (
     <SectionContainer>
       <div className="flex flex-col justify-between h-screen">
         <header className="flex items-center justify-between py-10">
           <div>
-            <Link href="/" aria-label="Tailwind CSS Blog">
+            <Link href="/" aria-label="Tim Corley's Web Portfolio and Blog">
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  {theme == 'light' || undefined ? <LightLogo /> : <DarkLogo />}
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-quicksand font-semibold sm:block">
