de36c08e2556c4b4c018c8c60c790e9a9f8b968f
Tim Corley <tcorley215@gmail.com>
2021-05-06 16:47:19 -0400

    adding new project

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,6 +6,12 @@ const projectsData = [
     imgSrc: '/static/images/selenium.svg',
     href: 'https://github.com/tim-corley/selenium-pytest',
   },
+  {
+    title: 'Social Web App (MERNG Stack)',
+    description: `A fullstack, modern web application that allows users to make posts, comments, and likes. Built using the MERNG stack (MongoDB, Express, React, Node, GraphQL) and entirely hosted in the cloud via Mongo Atlas, Heroku, and Netlify.`,
+    imgSrc: '/static/images/graphql.svg',
+    href: 'https://vigilant-wing-933e71.netlify.app/',
+  },
 ]
 
 export default projectsData
