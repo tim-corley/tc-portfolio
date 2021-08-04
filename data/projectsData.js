241a649c04df8b28e126d07c86d2c572c260a95f
Tim Corley <tcorley215@gmail.com>
2021-08-04 11:53:11 -0400

    adding new item to  project page (code launch site)

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,13 +1,5 @@
 const projectsData = [
   {
-    title: 'Automated UI Testing',
-    description: `A foundation for getting started with Automated UI Testing using Selenium & PyTest. This project contains 
-    the configurations, page object models, and example tests needed to get up-and-running with any web application.`,
-    imgSrc: '/static/images/selenium.svg',
-    code: 'https://github.com/tim-corley/selenium-pytest',
-    demo: '',
-  },
-  {
     title: 'Social Web App (MERNG Stack)',
     description: `A fullstack, modern web application that allows users to make posts, comments, and likes. Built using the MERNG stack (MongoDB, Express, React, Node, GraphQL) and entirely hosted in the cloud via Mongo Atlas, Heroku, and Netlify.`,
     imgSrc: '/static/images/graphql.svg',
@@ -27,6 +19,21 @@ const projectsData = [
     imgSrc: '/static/images/typescript.svg',
     code: 'https://github.com/tim-corley/react-ts-quiz-app',
     demo: 'tim-corley.github.io/react-ts-quiz-app/',
+  },
+  {
+    title: '"Code Launch" Marketing Site',
+    description: `A static marketing website for a demo product that leverages CSS Grid, Flexbox, and Media Queries to be fully responsive and user friendly.`,
+    imgSrc: '/static/images/css-3.svg',
+    code: 'https://github.com/tim-corley/code-launch-site',
+    demo: 'https://reverent-hermann-0912ed.netlify.app/index.html',
+  },
+  {
+    title: 'Automated UI Testing',
+    description: `A foundation for getting started with Automated UI Testing using Selenium & PyTest. This project contains 
+    the configurations, page object models, and example tests needed to get up-and-running with any web application.`,
+    imgSrc: '/static/images/selenium.svg',
+    code: 'https://github.com/tim-corley/selenium-pytest',
+    demo: '',
   },
 ]
 
