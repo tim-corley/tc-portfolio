55b269773adb56bac63c4b0af50d33db5a92c447
Tim Corley <tcorley215@gmail.com>
2025-08-12 16:04:24 -0400

    fix async func in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,10 +3,10 @@ import { getBlogPosts } from 'app/blog/utils'
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
+  const blogs = (await getBlogPosts()).map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
-  }))
+  }));
 
   let routes = ['', '/blog'].map((route) => ({
     url: `${baseUrl}${route}`,
