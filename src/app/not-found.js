9f681f4085048a7b4fd8173d356463ff72371fc6
Tim Corley <tcorley215@gmail.com>
2025-08-12 12:23:17 -0400

    update not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="p-5 text-center">
+      <h1 className="font-title text-4xl mt-12">404 | Page Not Found</h1>
+      <p className="font-body font-thin mt-4">Sorry, we couldn't find the page you're looking for.</p>
+    </div>
+  );
+}
